5696cf11d8644e850c8c40d140da8e7b318ae2df
seragithantsha <tiishetsoseragi@gmail.com>
2025-04-17 18:29:46 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+	base: "/app-project/", // Replace with your repository name
 	plugins: [react(), tailwindcss()],
 });
